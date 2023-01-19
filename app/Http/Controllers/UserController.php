810c1f7a0563f2644b2918a9d8a0fe64ed0d23f6
sahrulprograming <sahrulsaefulah@gmail.com>
2023-01-19 19:39:28 +0700

    membuat CRUD Users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Helper;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -16,20 +20,11 @@ class UserController extends Controller
     public function index()
     {
         $users = User::all();
+        $users = collect($users)->map->only(['id', 'picture', 'full_name', 'email', 'created_at', 'updated_at', 'role_user'])->all();
         return response()->json([
-            'user' => $users,
+            'users' => $users,
             'message' => 'data user berhasil di ambil'
-        ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        ], 200);
     }
 
     /**
@@ -40,7 +35,34 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'full_name' => 'required|string|max:45',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                "message" => $validator->errors()
+            ], 400);
+        }
+        $user = new User;
+        if ($request->file('picture')) {
+            $path = Storage::putFile('public/images/profile', $request->image);
+            $path = Helper::reImagePath($path);
+            $user->picture = $path;
+        }
+        $user->full_name = $request->full_name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->role_user_id = 2;
+        $user->save();
+        $user = collect($user)->only(['id', 'full_name', 'email', 'picture'])->put('role_name', $user->role_user->role_name);
+        return response()->json([
+            'message' => 'Registrasi Berhasil',
+            'user' => $user,
+        ], 201);
     }
 
     /**
@@ -66,7 +88,37 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $rules = [
+            'full_name' => 'required|string|max:45',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                "message" => $validator->errors()
+            ], 400);
+        }
+        $user = new User;
+        if ($request->file('picture')) {
+            $path = Storage::putFile('public/images/profile', $request->picture);
+            $path = Helper::reImagePath($path);
+            $user->picture = $path;
+            if ($request->picture != 'default.jpg') {
+                unlink(public_path('storage/images/' . $request->picture));
+            }
+        }
+        $user->full_name = $request->full_name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->role_user_id = 2;
+        $user->save();
+        $user = collect($user)->only(['id', 'full_name', 'email', 'picture'])->put('role_name', $user->role_user->role_name);
+        return response()->json([
+            'message' => 'Registrasi Berhasil',
+            'user' => $user,
+        ], 200);
     }
 
     /**
@@ -77,6 +129,10 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return response()->json([
+            'city' => null,
+            'message' => 'Data user berhasil dihapus'
+        ], 204);
     }
 }
